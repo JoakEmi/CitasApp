edc1f29d15442178de4600045c986856b04eb53f
JoakEmi <emiliano.pdz13@gmail.com>
2023-10-31 07:55:27 -0600

    Seccion06 - Added routing in SPA

--- CitasAppClient/src/app/nav/nav.component.ts
+++ CitasAppClient/src/app/nav/nav.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { IUser } from '../_models/iuser';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -9,25 +12,25 @@ import { Observable, of } from 'rxjs';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit{
-  model: any = {};
-  currentUser$: Observable<IUser | null> = of(null);
+  model: any = {}
 
-  constructor(private accountService: AccountService) {}
+  constructor(public accountService: AccountService, 
+    private router: Router, 
+    private toastr: ToastrService) {}
 
   ngOnInit(): void {
-    this.currentUser$ = this.accountService.currentUser$;
+    //this.currentUser$ = this.accountService.currentUser$;
   }
 
   login(): void {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
-      error: error => console.log(error)
+      next: _ => this.router.navigateByUrl("/members"),
+      error: error => this.toastr.error(error.error)
     })
   }
 
   logout(): void{
     this.accountService.logout();
+    this.router.navigateByUrl("/");
   }
 }
