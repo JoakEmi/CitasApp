a96b9238b8b3597135dcbe8dd3231dd5d2c57659
JoakEmi <emiliano.pdz13@gmail.com>
2023-11-18 15:58:45 -0600

    Seccion09 - Added member cards

---
+++ CitasAppClient/src/app/_models/imember.ts
@@ -0,0 +1,19 @@
+import { IPhoto } from "./Iphoto";
+
+export interface IMember {
+    id: number;
+    userName: string;
+    photoUrl: string;
+    age: number;
+    knownAs: string;
+    created: Date;
+    lastActive: Date;
+    gender: string;
+    introduction: string;
+    lookingFor: string;
+    interests: string;
+    city: string;
+    country: string;
+    photos: IPhoto[];
+  }
+  
