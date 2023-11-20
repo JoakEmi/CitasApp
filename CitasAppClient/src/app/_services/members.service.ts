ce7301d543a558db79c7b26b28dbab3b74c66bea
JoakEmi <emiliano.pdz13@gmail.com>
2023-11-20 08:54:06 -0600

    Seccion09 - Added JWT Interceptor

--- CitasAppClient/src/app/_services/members.service.ts
+++ CitasAppClient/src/app/_services/members.service.ts
@@ -14,24 +14,12 @@ export class MembersService {
   
   getMembers()
   {
-      return this.http.get<IMember[]>(this.baseUrl + "users", this.getHttpOptions());
+      return this.http.get<IMember[]>(this.baseUrl + "users");
   }
 
   getMember(username: string) 
   {
-      return this.http.get<IMember>(this.baseUrl + "users/" + username, this.getHttpOptions());
-  }
-
-  getHttpOptions() 
-  {
-      const userString = localStorage.getItem("users");
-      if (!userString) return;
-      const user = JSON.parse(userString);
-      return {
-        headers: new HttpHeaders({
-          Authorization: "Bearer " + user.token
-        })
-      }
+      return this.http.get<IMember>(this.baseUrl + "users/" + username);
   }
   
 }
